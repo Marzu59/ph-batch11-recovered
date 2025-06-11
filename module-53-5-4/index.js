867fdcfe38d76ac9b80b8cc116201e0307aa514a
nazmul <marzu322@outlook.com>
2025-06-11 17:01:49 +0600

    afa

---
+++ module-53-5-4/index.js
@@ -0,0 +1,26 @@
+const  express = require('express');
+const cors = require('cors')
+const phones = require('./pns.json')
+const app = express();
+const port = 5000
+
+app.use(cors())
+app.get('/', (req, res)=>{
+
+    res.send("my information not coming soon")
+
+})
+
+app.get('/phones', (req, res)=>{
+    res.send(phones)
+})
+
+app.get('/phones/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+    // console.log('i need id', id)
+
+    const phone = phones.find(phone=> phone.id === id) || {};
+    res.send(phone)
+})
+
+app.listen(port)
